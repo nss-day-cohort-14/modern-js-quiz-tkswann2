b31a3fea5b809048e7bc491308f798c9d9b0fb15
Thomas Swann <tkswann@mac.com>
2016-07-12 00:57:52 -0500

    added selected style to weapons, next button, helperfuncs

--- js/domHandler.js
+++ js/domHandler.js
@@ -8,35 +8,50 @@ console.log("Class", Class);
 console.log("Weapons", Weapons);
 console.log("Bonus", Bonus);
 
-
+// dynamically load weapon choices
 $('#classSelect1, #classSelect2')
 	.on('change', (e) => {
-		let htmlString = `<p>${e.target.value}</p>`;
+		let htmlString = `<p>Select one:</p>`;
 		$.each(Weapons, (k,v) => {
-			htmlString += `<div class="card__button">
-	      							  <a class="card__link btn btn--small btn--yellow" href="">
-	      							    <span class="btn__prompt">&raquo;</span>
+			htmlString += `<div class="card__button">`;
+			// different color list buttons for each select box
+			htmlString +=	(e.target.id === 'classSelect1') ? `<a class="card__link btn btn--small btn--yellow weapons" href="#">` : `<a class="card__link btn btn--small btn--blue weapons" href="#">`;
+	    htmlString += `<span class="btn__prompt">&raquo;</span>
 	      							    <span class="btn__text">${k}</span>
 	      							  </a>
 	      							</div>
 										</div>`;
 		});
-		htmlString += `<div class="card__button">
-	      							  <a class="card__link btn btn--small btn--red" href="">
-	      							    <span class="btn__prompt">&raquo;</span>
-	      							    <span class="btn__text">Next</span>
-	      							  </a>
-	      							</div>
-										</div>`;
 		
 	 if (e.target.id === 'classSelect1') {
-	  $('#box-01').children(':not(label, select)').remove(); 
-	  $('#box-01').append(htmlString);
-	}
+	  	$('#box-01').children(':not(label, select)').remove(); 
+	  	$('#box-01').append(htmlString);
+		// change color of selected box
+	  	$('.btn--yellow').on('click', (e) => {
+				$('.btn--yellow').removeClass('btn__select');
+				$(e.currentTarget).toggleClass('btn__select');
+			});
+	 }
 	 if (e.target.id === 'classSelect2') {
-	  $('#box-03').children(':not(label, select)').remove();
-	  $('#box-03').append(htmlString);
-	} 
+	  	$('#box-03').children(':not(label, select)').remove();
+	  	$('#box-03').append(htmlString);
+		// change color of selected box
+	  	$('.btn--blue').on('click', (e) => {
+				$('.btn--blue').removeClass('btn__select');
+				$(e.currentTarget).toggleClass('btn__select');
+			});
+	 } 
+// prevent default action of dynamically created links
+	$('a').on('click', (e) => e.preventDefault());
+	
 });
+
+$('body').on('click', (e) => {
+	if (e.target.innerText === 'next') {
+		$('#classSelect1, #classSelect2').nextAll().remove();
+	}
+});
+
+
 
 
